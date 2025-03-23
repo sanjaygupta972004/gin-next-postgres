77250c62a7977c84b33aed094fe4365d4e8d83ad
savvy-bit <palindrome0223@gmail.com>
2025-03-23 10:28:29 -0700

    feat: add load spinner during the authenticataion

--- client/components/layouts/index.tsx
+++ client/components/layouts/index.tsx
@@ -1,19 +1,26 @@
 "use client";
 import { ToastContainer } from "react-toastify";
 import Header from "./header";
+import { useAuth } from "@/context/AuthContext";
+import { InfinitySpin } from "react-loader-spinner";
 
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
+  const { isLoading } = useAuth();
   return (
     <>
       <div className="w-screen min-h-screen flex flex-col">
         <Header />
         <div className="flex-1 flex flex-col max-w-[1440px] w-full m-auto p-8">
-          {children}
+          {isLoading ?
+            <div className="flex justify-center items-center m-auto">
+              <InfinitySpin color="#FFF" />
+            </div>
+            : children
+          }
         </div>
         <footer className="border-t border-dashed border-t-zinc-800 py-4">
           <p className="text-sm text-center font-semibold">Copyright</p>
